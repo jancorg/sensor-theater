3df2d773d16c9817110a78d7ba81c7245b5e4e3f
jac <systems@finect.com>
2013-12-26 16:37:59 +0100

    Arduino file added. First test with just one sensor/potenciometer.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ sensor-theater
 ==============
 
 Android app that collects and display data from bt connected arduino board.
+
+Arduino boards is really simple at the moment. Just a bt device and a potentiomenter.
+
+Arduino app will send potentiometer status trough bt 10 times per second. 
+
+Android app will connect to cutom board and display potentiometer status in a graph view. 
+One input, one output, for now.
---
+++ arduino/datacast.ino
@@ -0,0 +1,50 @@
+#include <SoftwareSerial.h>
+
+#define RX 3
+#define TX 2
+
+#define LED 13
+#define POT 0
+
+#include <MeetAndroid.h>
+
+MeetAndroid meetAndroid;
+
+SoftwareSerial BTSerial(RX, TX);
+
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(POT, INPUT); // There is no other mode for pins A0-A5, so no config needed
+  
+  digitalWrite(LED, LOW).
+  // Software serial HC-05 setup (Easier for frecuent flashes and debug ;))
+  BTSerial.begin(9600);
+  BTSerial.flush();
+  delay(500);
+  
+  // Regular Serial is for debug 
+  Serial.begin(9600);
+  Serial.println("Setup Done. Go on!");
+}
+
+void loop()
+{
+  /*
+  if (BTSerial.available()){
+    char command = BTSerial.read();
+    BTSerial.flush();
+    Serial.println(command);
+  }*/
+  
+  int pot_status;
+  pot_status = analogRead(POT);
+  /*
+  BTSerial.write(pot_status);
+  */
+  meetAndroid.receive();
+  meetAndroid.send(pot_status);
+
+  delay(100);
+}
